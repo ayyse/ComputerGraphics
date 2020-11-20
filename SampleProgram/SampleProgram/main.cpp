43297d78553c172106a6ddb816ac275c4b28a73f
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2020-11-20 15:55:50 +0300

    Add files via upload

---
+++ SampleProgram/SampleProgram/main.cpp
@@ -0,0 +1,42 @@
+#include<GL/glut.h>
+
+void init(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+
+	glMatrixMode(GL_PROJECTION);
+
+	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+}
+
+void lineSegment(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1.0, 0.0, 0.0);
+
+	glBegin(GL_LINES);
+		glVertex2i(180, 15);
+		glVertex2i(10, 145);
+		glEnd();
+
+		glFlush();
+}
+
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitWindowPosition(50, 100);
+
+	glutInitWindowSize(400, 300);
+
+	glutCreateWindow("An example opengl program");
+
+	init();
+
+	glutDisplayFunc(lineSegment);
+
+	glutMainLoop();
+}
+
